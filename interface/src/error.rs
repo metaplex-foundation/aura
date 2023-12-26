4b5685fa8a439da8c3f294aa72a45335e2d9593e
Stanislav Cherviakov <stchervyakov@gmail.com>
2023-12-26 13:40:33 +0000

    usecase to validate request

--- interface/src/error.rs
+++ interface/src/error.rs
@@ -1,8 +1,12 @@
 use thiserror::Error;
 
 // TODO: rename the error enum as soon as it gets at least 3 errors
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq)]
 pub enum UsecaseError {
     #[error("The range is invalid. Start slot {0} is greater than end slot {1}.")]
     InvalidRange(u64, u64),
+    #[error(
+        "The range is too wide. Start slot {0} and end slot {1} are more than {2} slots apart."
+    )]
+    InvalidRangeTooWide(u64, u64, u64),
 }
