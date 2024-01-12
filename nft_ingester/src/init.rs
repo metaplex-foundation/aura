fcc3c2a466fa516b9d16bb143ec57598b971175a
requesco <svxoma1@gmail.com>
2024-01-12 15:30:59 +0200

    feat: profiling

--- nft_ingester/src/init.rs
+++ nft_ingester/src/init.rs
@@ -1,4 +1,8 @@
 use log::{error, info};
+use pprof::protos::Message;
+use pprof::ProfilerGuard;
+use std::fs::File;
+use std::io::Write;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use tokio::signal;
@@ -7,10 +11,12 @@ use tokio::sync::Mutex;
 use tokio::task::{JoinError, JoinSet};
 
 pub async fn graceful_stop(
-    tasks: Arc<Mutex<JoinSet<core::result::Result<(), JoinError>>>>,
+    tasks: Arc<Mutex<JoinSet<Result<(), JoinError>>>>,
     wait_all: bool,
     keep_running: Arc<AtomicBool>,
     shutdown_tx: Sender<()>,
+    guard: Option<ProfilerGuard<'_>>,
+    profile_path: Option<String>,
 ) {
     match signal::ctrl_c().await {
         Ok(()) => {}
@@ -19,6 +25,17 @@ pub async fn graceful_stop(
         }
     }
     keep_running.store(false, Ordering::SeqCst);
+
+    if let Some(guard) = guard {
+        if let Ok(report) = guard.report().build() {
+            // This code will be called only for profiling, so unwraps is used
+            let mut file = File::create(format!("{}/profile.pb", profile_path.unwrap())).unwrap();
+            let profile = report.pprof().unwrap();
+
+            let content = profile.write_to_bytes().unwrap();
+            file.write_all(&content).unwrap();
+        }
+    }
 
     let _ = shutdown_tx.send(());
     while let Some(task) = tasks.lock().await.join_next().await {
