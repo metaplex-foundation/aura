9501fdaadfd0d3e517209fc2edf785f0538fca2b
requesco <svxoma1@gmail.com>
2024-01-23 10:37:15 +0200

    change indexes

--- init_v3.sql
+++ init_v3.sql
@@ -80,32 +80,32 @@ CREATE TABLE assets_v3 (
 );
 -- indexes on the fields that will not get updated:
 -- so far we only know of V1 specification versions, so we only index on others
-CREATE INDEX assets_v3_specification_version ON assets_v3 (ast_specification_version) WHERE ast_specification_version <> 'v1'::specification_versions AND ast_metadata_present IS TRUE;
-CREATE INDEX assets_v3_specification_asset_class ON assets_v3 (ast_specification_asset_class) WHERE ast_specification_asset_class IS NOT NULL AND ast_specification_asset_class <> 'unknown'::specification_asset_class AND ast_metadata_present IS TRUE;
+CREATE INDEX assets_v3_specification_version ON assets_v3 (ast_specification_version, ast_metadata_present) WHERE ast_specification_version <> 'v1'::specification_versions;
+CREATE INDEX assets_v3_specification_asset_class ON assets_v3 (ast_specification_asset_class, ast_metadata_present) WHERE ast_specification_asset_class IS NOT NULL AND ast_specification_asset_class <> 'unknown'::specification_asset_class;
 -- so far we only know of creators as royalty target type, so we only index on others
-CREATE INDEX assets_v3_royalty_target_type ON assets_v3 (ast_royalty_target_type) WHERE ast_royalty_target_type <> 'creators'::royalty_target_type AND ast_metadata_present IS TRUE;
+CREATE INDEX assets_v3_royalty_target_type ON assets_v3 (ast_royalty_target_type, ast_metadata_present) WHERE ast_royalty_target_type <> 'creators'::royalty_target_type;
 -- so far I've seen only null royalty targets, this should be verified
-CREATE INDEX assets_v3_royalty_amount ON assets_v3 (ast_royalty_amount) WHERE ast_metadata_present IS TRUE;
-CREATE INDEX assets_v3_slot_created ON assets_v3 (ast_slot_created) WHERE ast_metadata_present IS TRUE;
-CREATE INDEX assets_v3_owner_type ON assets_v3 (ast_owner_type) WHERE ast_owner_type IS NOT NULL AND ast_owner_type <> 'unknown'::owner_type AND ast_metadata_present IS TRUE;
+CREATE INDEX assets_v3_royalty_amount ON assets_v3 (ast_royalty_amount, ast_metadata_present);
+CREATE INDEX assets_v3_slot_created ON assets_v3 (ast_slot_created, ast_metadata_present);
+CREATE INDEX assets_v3_owner_type ON assets_v3 (ast_owner_type, ast_metadata_present) WHERE ast_owner_type IS NOT NULL AND ast_owner_type <> 'unknown'::owner_type;
 
 -- indexes on the fields that will get updated:
 -- a conditional index on ast_metadata_url_id where ast_metadata_url_id is not null
-CREATE INDEX assets_v3_metadata_url ON assets_v3 (ast_metadata_url_id) WHERE ast_metadata_url_id IS NOT NULL AND ast_metadata_present IS TRUE;
-CREATE INDEX assets_v3_owner ON assets_v3(ast_owner) WHERE ast_owner IS NOT NULL AND ast_metadata_present IS TRUE;
-CREATE INDEX assets_v3_delegate ON assets_v3(ast_delegate) WHERE ast_delegate IS NOT NULL AND ast_metadata_present IS TRUE;
-CREATE INDEX assets_v3_authority ON assets_v3(ast_authority) WHERE ast_authority IS NOT NULL AND ast_metadata_present IS TRUE;
-CREATE INDEX assets_v3_collection_is_collection_verified ON assets_v3(ast_collection, ast_is_collection_verified) WHERE ast_collection IS NOT NULL AND ast_metadata_present IS TRUE;
+CREATE INDEX assets_v3_metadata_url ON assets_v3 (ast_metadata_url_id, ast_metadata_present) WHERE ast_metadata_url_id IS NOT NULL;
+CREATE INDEX assets_v3_owner ON assets_v3(ast_owner, ast_metadata_present) WHERE ast_owner IS NOT NULL;
+CREATE INDEX assets_v3_delegate ON assets_v3(ast_delegate, ast_metadata_present) WHERE ast_delegate IS NOT NULL;
+CREATE INDEX assets_v3_authority ON assets_v3(ast_authority, ast_metadata_present) WHERE ast_authority IS NOT NULL;
+CREATE INDEX assets_v3_collection_is_collection_verified ON assets_v3(ast_collection, ast_is_collection_verified, ast_metadata_present) WHERE ast_collection IS NOT NULL;
 -- as 95% of assets are not burnt, we only index on burnt assets
-CREATE INDEX assets_v3_is_burnt ON assets_v3(ast_is_burnt) WHERE ast_is_burnt IS TRUE AND ast_metadata_present IS TRUE;
+CREATE INDEX assets_v3_is_burnt ON assets_v3(ast_is_burnt, ast_metadata_present) WHERE ast_is_burnt IS TRUE;
 -- all observed assets are not compressible, so we only index on compressible assets
-CREATE INDEX assets_v3_is_compressible ON assets_v3(ast_is_compressible) WHERE ast_is_compressible IS TRUE AND ast_metadata_present IS TRUE;
-CREATE INDEX assets_v3_is_compressed ON assets_v3(ast_is_compressed) WHERE ast_metadata_present IS TRUE;
+CREATE INDEX assets_v3_is_compressible ON assets_v3(ast_is_compressible, ast_metadata_present) WHERE ast_is_compressible IS TRUE;
+CREATE INDEX assets_v3_is_compressed ON assets_v3(ast_is_compressed, ast_metadata_present);
 -- all observed assets are not frozen, so we only index on frozen assets
-CREATE INDEX assets_v3_is_frozen ON assets_v3(ast_is_frozen) WHERE ast_is_frozen IS TRUE AND ast_metadata_present IS TRUE;
+CREATE INDEX assets_v3_is_frozen ON assets_v3(ast_is_frozen, ast_metadata_present) WHERE ast_is_frozen IS TRUE;
 
-CREATE INDEX assets_v3_supply ON assets_v3(ast_supply) WHERE ast_supply IS NOT NULL AND ast_metadata_present IS TRUE;
-CREATE INDEX assets_v3_slot_updated ON assets_v3(ast_slot_updated) WHERE ast_metadata_present IS TRUE;
+CREATE INDEX assets_v3_supply ON assets_v3(ast_supply, ast_metadata_present) WHERE ast_supply IS NOT NULL;
+CREATE INDEX assets_v3_slot_updated ON assets_v3(ast_slot_updated, ast_metadata_present);
 
 CREATE TABLE tasks (
     tsk_id bigserial
