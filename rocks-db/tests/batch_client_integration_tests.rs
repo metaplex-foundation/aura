d9736f9447d121de22de3ba5a6de05abc5410bc0
Vadim <grozinok@gmail.com>
2024-01-30 14:12:19 +0100

    fix: test

--- rocks-db/tests/batch_client_integration_tests.rs
+++ rocks-db/tests/batch_client_integration_tests.rs
@@ -392,7 +392,8 @@ mod tests {
         let selected_data = storage.asset_dynamic_data.get(pk).unwrap().unwrap();
         assert_eq!(
             selected_data.is_compressible,
-            Updated::new(5, Some(1), false)
-        );
+            Updated::new(10, None, true)
+        ); // data will not be updated because slot is lower
+        // and to update data based of seq both new and old records have to have that value
     }
 }
