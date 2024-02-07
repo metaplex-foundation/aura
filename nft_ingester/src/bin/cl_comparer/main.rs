157ccedaad11f7b9b0d31eee14e892d8e9231531
Stanislav Cherviakov <stchervyakov@gmail.com>
2024-02-07 18:06:07 +0000

    compare items

--- nft_ingester/src/bin/cl_comparer/main.rs
+++ nft_ingester/src/bin/cl_comparer/main.rs
@@ -22,11 +22,10 @@ struct Args {
 
 #[tokio::main(flavor = "multi_thread")]
 pub async fn main() -> Result<(), IngesterError> {
-    
     let config: TreeBackfillerConfig = setup_config(COMPARER_CONFIG_PREFIX);
     init_logger(&config.get_log_level());
     tracing::info!("Starting comparer");
-    
+
     let tasks = JoinSet::new();
     let mutexed_tasks = Arc::new(Mutex::new(tasks));
 
@@ -37,7 +36,6 @@ pub async fn main() -> Result<(), IngesterError> {
     )
     .unwrap();
 
-
     let second_rocks = Storage::open_as_secondary(
         &config.source_rocks.clone(),
         "./secondary2",
@@ -46,6 +44,11 @@ pub async fn main() -> Result<(), IngesterError> {
     .unwrap();
 
     let target_tree_key = Pubkey::from_str(&config.target_tree_key).unwrap();
+    compare_cl_items(first_rocks, second_rocks, target_tree_key);
+    Ok(())
+}
+
+fn compare_leaves(first_rocks: Storage, second_rocks: Storage, target_tree_key: Pubkey) {
     let first_map = first_rocks
         .cl_leafs
         .iter_deserialized()
@@ -110,6 +113,72 @@ pub async fn main() -> Result<(), IngesterError> {
     }
 
     info!("Finished comparing the leaves");
+}
 
-    Ok(())
+fn compare_cl_items(first_rocks: Storage, second_rocks: Storage, target_tree_key: Pubkey) {
+    let first_map = first_rocks
+        .cl_items
+        .iter_deserialized()
+        .filter_map(|v| v.ok())
+        .filter(|((_, tree_id), _)| *tree_id == target_tree_key)
+        .map(|((node_id, _), value)| (node_id, value))
+        .collect::<HashMap<_, _>>();
+
+    tracing::info!(
+        "Collected the items for the source. Total items for the first map: {}",
+        first_map.len(),
+    );
+
+    let second_map = second_rocks
+        .cl_items
+        .iter_deserialized()
+        .filter_map(|v| v.ok())
+        .filter(|((_, tree_id), _)| *tree_id == target_tree_key)
+        .map(|((node_id, _), value)| (node_id, value))
+        .collect::<HashMap<_, _>>();
+    tracing::info!(
+        "Collected all the items. Total items for the first map: {}, for the second map: {}",
+        first_map.len(),
+        second_map.len()
+    );
+
+    for (node_id, first_item) in first_map.iter() {
+        if let Some(second_item) = second_map.get(node_id) {
+            if first_item != second_item {
+                tracing::info!(
+                    "Found a different item at node {}: {:?} vs {:?}",
+                    node_id,
+                    first_item,
+                    second_item
+                );
+            }
+        } else {
+            tracing::info!(
+                "Found a item in the first map that is not in the second map at node {}: {:?}",
+                node_id,
+                first_item
+            );
+        }
+    }
+
+    for (node_id, second_item) in second_map.iter() {
+        if let Some(first_item) = first_map.get(node_id) {
+            if first_item != second_item {
+                tracing::info!(
+                    "Found a different item at node {}: {:?} vs {:?}",
+                    node_id,
+                    first_item,
+                    second_item
+                );
+            }
+        } else {
+            tracing::info!(
+                "Found a item in the second map that is not in the first map at node {}: {:?}",
+                node_id,
+                second_item
+            );
+        }
+    }
+
+    info!("Finished comparing the items");
 }
