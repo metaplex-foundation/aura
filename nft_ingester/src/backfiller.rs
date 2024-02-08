32afe683c99c44917db61bf921f943a0aad44221
requesco <svxoma1@gmail.com>
2024-02-08 14:13:24 +0200

    use finalized slot in perpetual_slot_collection

--- nft_ingester/src/backfiller.rs
+++ nft_ingester/src/backfiller.rs
@@ -4,6 +4,7 @@ use async_trait::async_trait;
 use entities::models::BufferedTransaction;
 use flatbuffers::FlatBufferBuilder;
 use futures::future::join_all;
+use interface::finalized_slot_getter::FinalizedSlotGetter;
 use interface::signature_persistence::{BlockConsumer, BlockProducer};
 use interface::slots_dumper::SlotGetter;
 use log::{error, info, warn};
@@ -62,7 +63,8 @@ impl Backfiller {
         &self,
         metrics: Arc<BackfillerMetricsConfig>,
         wait_period: Duration,
-        mut rx: tokio::sync::broadcast::Receiver<()>,
+        finalized_slot_getter: Arc<impl FinalizedSlotGetter>,
+        mut rx: Receiver<()>,
     ) -> Result<(), IngesterError> {
         info!("Starting perpetual slot parser");
 
@@ -80,9 +82,10 @@ impl Backfiller {
         if let Some(slot) = top_collected_slot {
             parse_until = slot;
         }
+        let finalized_slot = finalized_slot_getter.get_finalized_slot().await?;
         loop {
             let top_collected_slot = slots_collector
-                .collect_slots(BBG_PREFIX, u64::MAX, parse_until, &rx)
+                .collect_slots(BBG_PREFIX, finalized_slot, parse_until, &rx)
                 .await;
             if let Some(slot) = top_collected_slot {
                 parse_until = slot;
@@ -113,7 +116,7 @@ impl Backfiller {
         block_consumer: Arc<C>,
         block_producer: Arc<P>,
         wait_period: Duration,
-        rx: tokio::sync::broadcast::Receiver<()>,
+        rx: Receiver<()>,
     ) -> Result<(), IngesterError>
     where
         C: BlockConsumer,
