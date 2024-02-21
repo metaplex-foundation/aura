d79ff773900eac8dfb7c646725aa51ec6e77e32e
requesco <svxoma1@gmail.com>
2024-02-21 23:14:18 +0200

    WIP: log && metric

--- nft_ingester/src/backfiller.rs
+++ nft_ingester/src/backfiller.rs
@@ -480,7 +480,8 @@ where
                     let block = match p.get_block(s).await {
                         Ok(block) => block,
                         Err(err) => {
-                            error!("Error getting block: {}", err);
+                            error!("Error getting block {}: {}", s, err);
+                            m.inc_data_processed("error_getting_block");
                             continue;
                         }
                     };
