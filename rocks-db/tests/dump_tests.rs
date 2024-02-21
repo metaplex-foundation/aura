cbc037598ed3c6c6b7f2fcafef865e590b7195a6
Stanislav Cherviakov <stchervyakov@gmail.com>
2024-02-21 09:24:16 +0000

    a test fixed

--- rocks-db/tests/dump_tests.rs
+++ rocks-db/tests/dump_tests.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{collections::HashSet, fs::File};
 
 use csv::WriterBuilder;
 use setup::rocks::*;
@@ -35,6 +35,7 @@ async fn test_scv_export_from_rocks() {
             &mut creators_writer,
             &mut assets_writer,
             155,
+            HashSet::new(),
             rx,
         )
         .await
