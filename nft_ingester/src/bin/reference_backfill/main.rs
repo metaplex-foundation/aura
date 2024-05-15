0bbe7274d95302f6d0748ddd9c391a328801f8f8
requesco <svxoma1@gmail.com>
2024-05-15 13:28:22 +0300

    fmt

--- nft_ingester/src/bin/reference_backfill/main.rs
+++ nft_ingester/src/bin/reference_backfill/main.rs
@@ -8,22 +8,19 @@ use tempfile::TempDir;
 use tokio::sync::{broadcast, Mutex};
 use tokio::task::JoinSet;
 
-const CONTAINER_DUMP_DIR: &str = "/usr/src/dump";
-
 #[tokio::main(flavor = "multi_thread")]
 pub async fn main() -> Result<(), IngesterError> {
     // Retrieve the database paths from command-line arguments
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         println!(
-            "Usage: {} <source_db_path> <reference_db_url> <dump_dir_path>",
+            "Usage: {} <source_db_path> <reference_db_url>",
             args[0]
         );
         std::process::exit(1);
     }
     let source_db_path = &args[1];
     let reference_db_url = &args[2];
-    let dump_dir_path = &args[3];
 
     let mutexed_tasks = Arc::new(Mutex::new(JoinSet::new()));
     let red_metrics = Arc::new(RequestErrorDurationMetrics::new());
@@ -39,11 +36,11 @@ pub async fn main() -> Result<(), IngesterError> {
 
     let reference_storage =
         Arc::new(PgClient::new(reference_db_url, 100, 250, red_metrics.clone()).await?);
-    // let dump_dir = TempDir::new().unwrap();
+    let dump_dir = TempDir::new().unwrap();
     println!("Starting creating dump...");
     source_storage
         .dump_reference_db(
-            std::path::Path::new(dump_dir_path.as_str()),
+            std::path::Path::new(dump_dir.path()),
             200_000,
             &shutdown_rx.resubscribe(),
         )
@@ -51,7 +48,7 @@ pub async fn main() -> Result<(), IngesterError> {
         .unwrap();
     println!("Starting insert dump...");
     reference_storage
-        .load_from_reference_dump(std::path::Path::new(CONTAINER_DUMP_DIR))
+        .load_from_reference_dump(dump_dir.path())
         .await
         .unwrap();
 
