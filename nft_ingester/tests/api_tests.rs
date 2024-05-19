211c433d161182e8ecce9576aa7cfb457d7bb5cd
requesco <svxoma1@gmail.com>
2024-05-19 19:16:02 +0300

    add test

--- nft_ingester/tests/api_tests.rs
+++ nft_ingester/tests/api_tests.rs
@@ -14,7 +14,9 @@ mod tests {
     use digital_asset_types::rpc::response::{
         AssetList, TokenAccountsList, TransactionSignatureList,
     };
-    use entities::api_req_params::{DisplayOptions, GetAssetSignatures, GetTokenAccounts, Options};
+    use entities::api_req_params::{
+        DisplayOptions, GetAssetProof, GetAssetSignatures, GetTokenAccounts,
+    };
     use entities::models::{AssetSignature, AssetSignatureKey, OffChainData};
     use entities::{
         api_req_params::{
@@ -38,6 +40,8 @@ mod tests {
         mplx_updates_processor::{BurntMetadataSlot, MetadataInfo, MplxAccsProcessor},
         token_updates_processor::TokenAccsProcessor,
     };
+    use rocks_db::asset::AssetLeaf;
+    use rocks_db::tree_seq::TreesGaps;
     use rocks_db::{
         columns::{Mint, TokenAccount},
         AssetAuthority, AssetDynamicDetails, AssetOwner, AssetStaticDetails,
@@ -47,6 +51,7 @@ mod tests {
     use solana_sdk::signature::Signature;
     use testcontainers::clients::Cli;
     use tokio::{sync::Mutex, task::JoinSet};
+    use usecase::error::DasApiError;
     use usecase::proofs::MaybeProofChecker;
 
     const SLOT_UPDATED: u64 = 100;
@@ -2221,5 +2226,55 @@ mod tests {
         assert_eq!(response["content"], expected_content);
 
         env.teardown().await;
+    }
+
+    #[tokio::test]
+    async fn test_cannot_service_gaped_tree() {
+        let cnt = 20;
+        let cli = Cli::default();
+        let (env, generated_assets) = setup::TestEnvironment::create(&cli, cnt, SLOT_UPDATED).await;
+        let api =
+            nft_ingester::api::api_impl::DasApi::<MaybeProofChecker, JsonWorker, JsonWorker>::new(
+                env.pg_env.client.clone(),
+                env.rocks_env.storage.clone(),
+                Arc::new(ApiMetricsConfig::new()),
+                None,
+                50,
+                None,
+                None,
+                JsonMiddlewareConfig::default(),
+            );
+        let asset_id = Pubkey::new_unique();
+        let tree_id = Pubkey::new_unique();
+        env.rocks_env
+            .storage
+            .asset_leaf_data
+            .put_async(
+                asset_id,
+                AssetLeaf {
+                    pubkey: asset_id,
+                    tree_id,
+                    leaf: None,
+                    nonce: None,
+                    data_hash: None,
+                    creator_hash: None,
+                    leaf_seq: None,
+                    slot_updated: 0,
+                },
+            )
+            .await
+            .unwrap();
+        env.rocks_env
+            .storage
+            .trees_gaps
+            .put_async(tree_id, TreesGaps {})
+            .await
+            .unwrap();
+        let payload = GetAssetProof {
+            id: asset_id.to_string(),
+        };
+        let res = api.get_asset_proof(payload).await.err().unwrap();
+
+        assert_eq!(res, DasApiError::CannotServiceRequest);
     }
 }
