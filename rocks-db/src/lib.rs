8848d08402a0a045ed76c6991432211691c7ebb0
Stanislav Cherviakov <stchervyakov@gmail.com>
2024-11-04 23:17:28 +0000

    reverted the raw slots removal

--- rocks-db/src/lib.rs
+++ rocks-db/src/lib.rs
@@ -316,6 +316,7 @@ impl Storage {
             Self::new_cf_descriptor::<asset::AssetsUpdateIdx>(migration_state),
             Self::new_cf_descriptor::<asset::SlotAssetIdx>(migration_state),
             Self::new_cf_descriptor::<signature_client::SignatureIdx>(migration_state),
+            Self::new_cf_descriptor::<RawBlock>(migration_state),
             Self::new_cf_descriptor::<parameters::ParameterColumn<u64>>(migration_state),
             Self::new_cf_descriptor::<bubblegum_slots::IngestableSlots>(migration_state),
             Self::new_cf_descriptor::<bubblegum_slots::ForceReingestableSlots>(migration_state),
