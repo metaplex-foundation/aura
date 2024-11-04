d5f2953517137d89fab1cc71e87804977a2e3722
Stanislav Cherviakov <stchervyakov@gmail.com>
2024-11-04 15:39:22 +0000

    turn the fork cleaner on/off from configuration

--- nft_ingester/src/config.rs
+++ nft_ingester/src/config.rs
@@ -175,6 +175,8 @@ pub struct IngesterConfig {
     pub rpc_retry_interval_millis: u64,
     #[serde(default)]
     pub run_sequence_consistent_checker: bool,
+    #[serde(default)]
+    pub run_fork_cleaner: bool,
     #[serde(default = "default_sequence_consistent_checker_wait_period_sec")]
     pub sequence_consistent_checker_wait_period_sec: u64,
     pub rpc_host: String,
