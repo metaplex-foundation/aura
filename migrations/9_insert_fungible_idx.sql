8d4b5d884e0ca9354c1ab708ce34337eb5b2db64
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-11-06 19:27:26 +0200

    Fixed migration

--- migrations/9_insert_fungible_idx.sql
+++ migrations/9_insert_fungible_idx.sql
@@ -1,3 +1,3 @@
 ALTER TABLE last_synced_key DROP CONSTRAINT only_one_row;
-INSERT INTO last_synced_key (null);
-ADD CONSTRAINT only_two_rows CHECK (id IN (1, 2));
+INSERT INTO last_synced_key (id, last_synced_asset_update_key) VALUES (2, NULL);
+ALTER TABLE last_synced_key ADD CONSTRAINT only_two_rows CHECK (id IN (1, 2));
