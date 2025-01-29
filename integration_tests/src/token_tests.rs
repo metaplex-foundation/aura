56f8b6c3f499be440ac681ebb293a29374f35240
andrii_kl <18900364+andrii-kl@users.noreply.github.com>
2025-01-29 22:01:25 +0100

    MTG-1029 write tests on determining non-fungible token type by token supply and decimals options.

--- integration_tests/src/token_tests.rs
+++ integration_tests/src/token_tests.rs
@@ -3,7 +3,10 @@ use std::sync::Arc;
 use entities::{api_req_params::GetAsset, enums::AssetType};
 use function_name::named;
 use itertools::Itertools;
-use nft_ingester::api::dapi::response::{AssetList, TokenAccountsList};
+use nft_ingester::api::dapi::{
+    response::{AssetList, TokenAccountsList},
+    rpc_asset_models::Asset,
+};
 use serial_test::serial;
 use tokio::{
     sync::{broadcast, Mutex},
@@ -41,16 +44,18 @@ async fn test_fungible_token_mint_freeze_authority() {
 
     let request: GetAsset = serde_json::from_str(request).unwrap();
     let response_value = setup.das_api.get_asset(request, mutexed_tasks.clone()).await.unwrap();
-    let res: AssetList = serde_json::from_value(response_value.clone()).unwrap();
+    let asset: Asset =
+        serde_json::from_value::<Asset>(response_value.clone()).expect("Cannot parse 'Asset'.");
+
+    assert_eq!(asset.clone().id, "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v".to_string());
+    assert_eq!(
+        asset.clone().token_info.unwrap().mint_authority.unwrap(),
+        "BJE5MMbqXjVwjAF7oxwPYXnTXDyspzZyt4vwenNw5ruG".to_string()
+    );
+    assert_eq!(
+        asset.clone().token_info.unwrap().freeze_authority.unwrap(),
+        "7dGbd2QZcCKcTndnHcTL8q7SMVXAkp688NTQYwrRCrar".to_string()
+    );
 
     insta::assert_json_snapshot!(name, response_value.clone());
-
-    // assert_eq!(
-    //     res.items[0].clone().token_info.unwrap().mint_authority.unwrap(),
-    //     "BJE5MMbqXjVwjAF7oxwPYXnTXDyspzZyt4vwenNw5ruG".to_string()
-    // );
-    // assert_eq!(
-    //     res.items[0].clone().token_info.unwrap().freeze_authority.unwrap(),
-    //     "7dGbd2QZcCKcTndnHcTL8q7SMVXAkp688NTQYwrRCrar".to_string()
-    // );
 }
