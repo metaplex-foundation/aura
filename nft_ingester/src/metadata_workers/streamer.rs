f526729f8fe70e53a75fcdf9ce0e2060a8cd5ce0
Kyrylo Stepanov <k.stepanov@tutanota.com>
2025-02-12 17:00:34 +0200

    remove redundant channel in streamer

--- nft_ingester/src/metadata_workers/streamer.rs
+++ nft_ingester/src/metadata_workers/streamer.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use entities::models::MetadataDownloadTask;
 use postgre_client::PgClient;
 use tokio::{
-    sync::{broadcast::Receiver, mpsc::Sender, Mutex},
+    sync::{broadcast::Receiver, mpsc::Sender},
     task::JoinSet,
     time::{sleep, Duration},
 };
@@ -18,8 +18,6 @@ pub struct TasksStreamer {
     pub shutdown_rx: Receiver<()>,
     pub pending_tasks_sender: Sender<MetadataDownloadTask>,
     pub refresh_tasks_sender: Sender<MetadataDownloadTask>,
-    // to check if it's empty and populate it
-    pub tasks_receiver: Arc<Mutex<Receiver<MetadataDownloadTask>>>,
 }
 
 impl TasksStreamer {
@@ -28,9 +26,8 @@ impl TasksStreamer {
         shutdown_rx: Receiver<()>,
         pending_tasks_sender: Sender<MetadataDownloadTask>,
         refresh_tasks_sender: Sender<MetadataDownloadTask>,
-        tasks_receiver: Arc<Mutex<Receiver<MetadataDownloadTask>>>,
     ) -> Self {
-        Self { db_conn, shutdown_rx, pending_tasks_sender, refresh_tasks_sender, tasks_receiver }
+        Self { db_conn, shutdown_rx, pending_tasks_sender, refresh_tasks_sender }
     }
 
     pub async fn run(self, num_of_tasks: i32, tasks: &mut JoinSet<()>) {
@@ -40,7 +37,6 @@ impl TasksStreamer {
 
     async fn stream_tasks(&self, task_type: TaskType, num_of_tasks: i32, tasks: &mut JoinSet<()>) {
         let mut shutdown_rx = self.shutdown_rx.resubscribe();
-        let tasks_receiver = self.tasks_receiver.clone();
         let db_conn = self.db_conn.clone();
         let tasks_sender = match task_type {
             TaskType::Pending => self.pending_tasks_sender.clone(),
@@ -51,32 +47,29 @@ impl TasksStreamer {
             tokio::select! {
                 _ = async move {
                     loop {
-                        let is_empty = tasks_receiver.lock().await.is_empty();
+                        let tasks = match task_type {
+                            TaskType::Pending => {
+                                db_conn.get_pending_metadata_tasks(num_of_tasks).await
+                            },
+                            TaskType::Refresh => {
+                                db_conn.get_refresh_metadata_tasks(num_of_tasks).await
+                            },
+                        }.map_err(|err| {
+                            error!("Error while selecting tasks for JsonDownloader: {}", err);
+                        });
 
-                        if is_empty {
-                            let tasks = match task_type {
-                                TaskType::Pending => {
-                                    db_conn.get_pending_metadata_tasks(num_of_tasks).await
-                                },
-                                TaskType::Refresh => {
-                                    db_conn.get_refresh_metadata_tasks(num_of_tasks).await
-                                },
-                            }.map_err(|err| {
-                                error!("Error while selecting tasks for JsonDownloader: {}", err);
-                            });
-
-                            if let Ok(tasks) = tasks {
-                                for task in tasks.iter() {
-                                    if let Err(err) = tasks_sender.send(task.clone()).await {
-                                        error!(
-                                            "Error during sending task to the tasks channel: {}",
-                                            err.to_string()
-                                        );
-                                    }
+                        if let Ok(tasks) = tasks {
+                            for task in tasks.iter() {
+                                if let Err(err) = tasks_sender.send(task.clone()).await {
+                                    error!(
+                                        "Error during sending task to the tasks channel: {}",
+                                        err.to_string()
+                                    );
                                 }
                             }
                         }
-                        sleep(Duration::from_secs(SLEEP_TIME)).await;
+
+                    sleep(Duration::from_secs(SLEEP_TIME)).await;
                     }
                 } => {},
                 _ = shutdown_rx.recv() => {},
