0fb235d2bb2950147176df0e7860fd6aabadd092
Kyrylo Stepanov <k.stepanov@tutanota.com>
2025-02-14 14:46:57 +0200

    get rid of redundant spawning threads inside of functions

--- nft_ingester/src/metadata_workers/streamer.rs
+++ nft_ingester/src/metadata_workers/streamer.rs
@@ -3,8 +3,7 @@ use std::sync::Arc;
 use entities::models::MetadataDownloadTask;
 use postgre_client::PgClient;
 use tokio::{
-    sync::{broadcast::Receiver, mpsc::Sender, Mutex},
-    task::JoinSet,
+    sync::{broadcast::Receiver, mpsc::Sender},
     time::{sleep, Duration},
 };
 use tracing::error;
@@ -30,19 +29,14 @@ impl TasksStreamer {
         Self { db_conn, shutdown_rx, pending_tasks_sender, refresh_tasks_sender }
     }
 
-    pub async fn run(self, num_of_tasks: i32, tasks: Arc<Mutex<JoinSet<()>>>) {
+    pub async fn run(self, num_of_tasks: i32) {
         tokio::join!(
-            self.stream_tasks(TaskType::Pending, num_of_tasks, tasks.clone()),
-            self.stream_tasks(TaskType::Refresh, num_of_tasks, tasks)
+            self.stream_tasks(TaskType::Pending, num_of_tasks),
+            self.stream_tasks(TaskType::Refresh, num_of_tasks)
         );
     }
 
-    async fn stream_tasks(
-        &self,
-        task_type: TaskType,
-        num_of_tasks: i32,
-        tasks: Arc<Mutex<JoinSet<()>>>,
-    ) {
+    async fn stream_tasks(&self, task_type: TaskType, num_of_tasks: i32) {
         let mut shutdown_rx = self.shutdown_rx.resubscribe();
         let db_conn = self.db_conn.clone();
         let tasks_sender = match task_type {
@@ -50,37 +44,35 @@ impl TasksStreamer {
             TaskType::Refresh => self.refresh_tasks_sender.clone(),
         };
 
-        tasks.lock().await.spawn(async move {
-            tokio::select! {
-                _ = async move {
-                    loop {
-                        let tasks = match task_type {
-                            TaskType::Pending => {
-                                db_conn.get_pending_metadata_tasks(num_of_tasks).await
-                            },
-                            TaskType::Refresh => {
-                                db_conn.get_refresh_metadata_tasks(num_of_tasks).await
-                            },
-                        }.map_err(|err| {
-                            error!("Error while selecting tasks for JsonDownloader: {}", err);
-                        });
+        tokio::select! {
+            _ = async move {
+                loop {
+                    let tasks = match task_type {
+                        TaskType::Pending => {
+                            db_conn.get_pending_metadata_tasks(num_of_tasks).await
+                        },
+                        TaskType::Refresh => {
+                            db_conn.get_refresh_metadata_tasks(num_of_tasks).await
+                        },
+                    }.map_err(|err| {
+                        error!("Error while selecting tasks for JsonDownloader: {}", err);
+                    });
 
-                        if let Ok(tasks) = tasks {
-                            for task in tasks.iter() {
-                                if let Err(err) = tasks_sender.send(task.clone()).await {
-                                    error!(
-                                        "Error during sending task to the tasks channel: {}",
-                                        err.to_string()
-                                    );
-                                }
+                    if let Ok(tasks) = tasks {
+                        for task in tasks.iter() {
+                            if let Err(err) = tasks_sender.send(task.clone()).await {
+                                error!(
+                                    "Error during sending task to the tasks channel: {}",
+                                    err.to_string()
+                                );
                             }
                         }
-
-                    sleep(Duration::from_secs(SLEEP_TIME)).await;
                     }
-                } => {},
-                _ = shutdown_rx.recv() => {},
-            }
-        });
+
+                sleep(Duration::from_secs(SLEEP_TIME)).await;
+                }
+            } => {},
+            _ = shutdown_rx.recv() => {},
+        }
     }
 }
