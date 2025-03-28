dba7a2601a7b87fe943c6b28d36f053864419b2e
Kyrylo Stepanov <k.stepanov@tutanota.com>
2025-03-28 14:13:14 +0200

    fix comments from ai

--- nft_ingester/src/metadata_workers/mod.rs
+++ nft_ingester/src/metadata_workers/mod.rs
@@ -4,6 +4,8 @@ pub mod persister;
 pub mod streamer;
 
 pub enum TaskType {
+    /// Used for new metadata that needs to be processed for the first time
     Pending,
+    /// Used for existing metadata that needs to be updated
     Refresh,
 }
